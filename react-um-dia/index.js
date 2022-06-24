2f7e9f11c2055cba180894fee032221db52dc985
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-06-24 12:48:31 -0300

    🖐️ Hello World, React

--- react-um-dia/index.js
+++ react-um-dia/index.js
@@ -1,2 +1,7 @@
-div_root = document.getElementById('root');
-div_root.innerHTML = "<h1>Hello World!</h1>";
+// div_root = document.getElementById('root');
+// div_root.innerHTML = "<h1>Hello World!</h1>";
+
+ReactDOM.render(
+    'Hello React!',
+    document.getElementById('root')
+);
